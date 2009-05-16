0d9b7e852344242d9f8c896ca0d2eaf7e1b66185
jmhanun <jmhanun@7d1e54de-8341-0410-81db-47b75ec2d254>
2009-05-16 21:27:48 +0000

    complete SetupT

--- jPackMRP/jPackMRP-ejb/src/java/ar/com/jpack/transferencia/SetupT.java
+++ jPackMRP/jPackMRP-ejb/src/java/ar/com/jpack/transferencia/SetupT.java
@@ -6,11 +6,68 @@
 package ar.com.jpack.transferencia;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  *
  * @author jmhanun
  */
 public class SetupT implements Serializable {
+    private Integer idSetup;
+    private String descripcion;
+    private String valor;
+    private Date fechaModificacion;
+    private UsuariosT idUsuario;
+
+    public SetupT() {
+    }
+
+    public SetupT(Integer idSetup, String descripcion, String valor, Date fechaModificacion, UsuariosT idUsuario) {
+        this.idSetup = idSetup;
+        this.descripcion = descripcion;
+        this.valor = valor;
+        this.fechaModificacion = fechaModificacion;
+        this.idUsuario = idUsuario;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(Date fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    public Integer getIdSetup() {
+        return idSetup;
+    }
+
+    public void setIdSetup(Integer idSetup) {
+        this.idSetup = idSetup;
+    }
+
+    public UsuariosT getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(UsuariosT idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
 
 }
